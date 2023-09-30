3fcb5405585401ed85dcc6e12e0324bfffe88349
cenmelt <clement.collet123@orange.fr>
2023-09-30 13:26:56 +0200

    Zone question terminé

--- src/components/zoneQuestion/zoneQuestion.js
+++ src/components/zoneQuestion/zoneQuestion.js
@@ -2,5 +2,5 @@ import styles from './zoneQuestion.module.sass'
 
 
 export default function zoneQuestion() {
-  return <div className={styles.zoneQuestion}></div>
+  return <div className={styles.gradient}><div className={styles.zoneQuestion}></div></div>
 }
