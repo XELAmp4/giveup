6923cee05f93cbbc043a71375344d28d64547470
cenmelt <clement.collet123@orange.fr>
2023-10-23 16:10:46 +0200

    resultat-timeline fonctionnel

--- src/components/Timeline/explication/explication-timeline.js
+++ src/components/Timeline/explication/explication-timeline.js
@@ -1,12 +1,19 @@
 import styles from './explication-timeline.module.sass'
+import Chrono from '../../chrono/chrono';
 
 
-export default function ExplicationTimeline() {
+
+export default function ExplicationTimeline({setGame}) {
   return( 
   <section className={styles.explication}>
     <h1>Petite explication</h1>
     <p>Le but est simple mettre dans l'ordre chronologique (du plus vieux au plus récent) les covers des albums. Pour changer la position de deux cover c'est simple il suffis simplement de cliquer sur les deux covers.</p>
     <img src='Timeline/exemple-timeline.png' alt='Screen du jeu'></img>
+    <Chrono 
+      chrono={5}
+      setGame={setGame}
+      redirection={'question-timeline'}
+    />
   </section>
   )
 }
--- src/components/Timeline/timeline.js
+++ src/components/Timeline/timeline.js
@@ -1,62 +1,61 @@
-import styles from './timeline.module.sass'
+import ExplicationTimeline from './explication/explication-timeline'
+import QuestionTimeline from './question/question-timeline'
+import ResultatTimeline from './resultat/resultat-timeline'
+import { useState } from 'react';
 import cover from '../../assets/data/cover.json'
-import { useState, useEffect } from 'react';
 
-// eslint-disable-next-line no-empty-pattern
+
 export default function Timeline({}) {
-    //fonction permettant de choisir trois images totalement aléatoirement
-    function random(){
-        var rand1 = Math.floor(Math.random()*21);    
-        var rand2 = Math.floor(Math.random()*21);    
-        while(rand1 === rand2){
-            rand2 = Math.floor(Math.random()*21); 
-        }
-        var rand3 = Math.floor(Math.random()*21);
-        while(rand2 === rand3 || rand1 === rand3){
-            rand3 = Math.floor(Math.random()*21); 
-        } 
-        return [rand1, rand2, rand3]
+  function random(nb){
+    var rand1 = Math.floor(Math.random()*nb);    
+    var rand2 = Math.floor(Math.random()*nb);    
+    while(rand1 === rand2){
+        rand2 = Math.floor(Math.random()*nb); 
     }
-    
-    //Ce tableau vide contiendra l'indices des deux cliques pour échanger les images
-    const [tab, setTab] = useState([]);
+    var rand3 = Math.floor(Math.random()*nb);
+    while(rand2 === rand3 || rand1 === rand3){
+        rand3 = Math.floor(Math.random()*nb); 
+    } 
+    return [rand1, rand2, rand3]
+  }
+  const source = random(21);
+  console.log(source, 'img')
 
-    const source = random();
-    //contient les 3 noms des fichiers des images
-    const [Sources, setSources] = useState([cover[source[0]].img, cover[source[1]].img, cover[source[2]].img]);
-    //Rentre les données des clique 
-    const MajTab = (indice)=>{
-        setTab([...tab, indice]);
-    }  
-    //Rafraichi le useState pour savoir tab est de longueur 2
-    useEffect(() => {
-        if(tab.length === 2){
-            const fsource = Sources;
-            let tmp = Sources[tab[1]];
-            fsource[tab[1]] = fsource[tab[0]];
-            fsource[tab[0]] = tmp;
-            setSources(fsource);
-            setTab([]);
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [tab])
+  //contient les 3 noms des fichiers des images
+  const [Sources, setSources] = useState([cover[source[0]], cover[source[1]], cover[source[2]]]);
 
-    //HTML
-  return (
-    <section className={styles.timeline}>
-        <h1>Question Timeline</h1>
-        <img className={`${styles.cover1} ${ tab.includes(0) ? styles.selected : ''}`} onClick={()=>MajTab(0)} src={'Timeline/'+Sources[0]} alt=''></img>
-        <img className={`${styles.cover2} ${ tab.includes(1) ? styles.selected : ''}`} onClick={()=>MajTab(1)} src={'Timeline/'+Sources[1]} alt=''></img>
-        <img className={`${styles.cover3} ${ tab.includes(2) ? styles.selected : ''}`} onClick={()=>MajTab(2)} src={'Timeline/'+Sources[2]} alt=''></img>
-        <div className={styles.fleche}>
-            <div className={styles.trait}></div>
-            <div className={styles.triangle}></div>
-        </div>
-        <div className={styles.texte}>
-            <p>Ancien</p>
-            <p>Récent</p>
-        </div>
-    </section>
-    )
+  const [gameT, setGame] = useState('explication-timeline');
+  let content;
+  switch (gameT) {
+    case 'explication-timeline':
+        content = (
+          <ExplicationTimeline
+            setGame={setGame}
+          />
+        )
+        break;
+    case 'question-timeline':
+        content = (
+          <QuestionTimeline
+          setGame={setGame}
+          Sources={Sources}
+          setSources={setSources}
+          />
+        )
+        break;
+    case 'resultat-timeline':
+        content = (
+          <ResultatTimeline
+            Sources={Sources}
+          />
+        )
+        break;
+    default:
+      break;
+  }
+  return(
+    <div>
+        {content}
+    </div>
+  )
 }
-
