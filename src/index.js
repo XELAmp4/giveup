83b103358f40e38a4c040c20946a971fc8115fcd
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-10-23 14:02:35 +0200

    version alpha d'alpha

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import {PartyProvider} from './providers/party-provider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <PartyProvider>
