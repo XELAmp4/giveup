1a19452e6198c76c5d2afa1b0a5fe3541712f628
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-10-23 16:10:17 +0200

    version puzzle grid

--- src/components/puzzle/imageDivide.js
+++ src/components/puzzle/imageDivide.js
@@ -1,37 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SortableContext, useSortable } from '@dnd-kit/sortable';
 import SortableImage from './sortableImage';
 import styles from './puzzle.module.sass';
 
 export default function ImageDivide({ elements }) {
-  const imgWidth = 150;
-  const imgHeight = 150;
-
-  useEffect(() => {
-    // La fonction imageLoad() est appelée une seule fois au chargement de la page.
-    imageLoad();
-  }, []);
-
-  function imageLoad() {
-    const newElements = [];
-    for (let row = 0; row < 3; row++) {
-      for (let col = 0; col < 3; col++) {
-        const style = {
-          backgroundImage: `url(https://musister.com/cdn/shop/files/garcon.png?v=1688509611&width=1946)`,
-          backgroundPosition: `-${col * imgWidth}px -${row * imgHeight}px`,
-          width: imgWidth,
-          height: imgHeight,
-        };
-        newElements.push({ id: `column_${col}_row_${row}`, style });
-      }
-    }
-    // setElements(newElements);
-  }
-
   return (
     <SortableContext items={elements.map((el) => el.id)}>
       {elements.map((data, index) => (
-        <SortableImage key={index} data={data} imgWidth={imgWidth} imgHeight={imgHeight} />
+        <SortableImage key={index} data={data} imgWidth={data.style.width} imgHeight={data.style.height} />
       ))}
     </SortableContext>
   );
--- src/components/puzzle/puzzle.js
+++ src/components/puzzle/puzzle.js
@@ -1,5 +1,4 @@
-// Puzzle.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   DndContext,
   closestCenter,
@@ -10,27 +9,54 @@ import {
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import ImageDivide from './imageDivide';
-import styles from './puzzle.module.sass'
+import styles from './puzzle.module.sass';
 
 export default function Puzzle() {
   const [elements, setElements] = useState([]);
+  
+  useEffect(() => {
+    const imgWidth = 150;
+    const imgHeight = 150;
+    const newElements = [];
+    let emp = 0;
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+       emp +=1
+        const style = {
+          backgroundImage: `url(https://musister.com/cdn/shop/files/garcon.png?v=1688509611&width=1946)`,
+          backgroundPosition: `-${col * imgWidth}px -${row * imgHeight}px`,
+          width: imgWidth,
+          height: imgHeight,
+        };
+        newElements.push({ id: `column_${col}_row_${row}`, class: `emp_${emp}`, style });
+      }
+    }
+
+    setElements(newElements);
+  }, []);
 
   const handleDragEnd = (event) => {
     const { active, over } = event;
+    
+    if (active.id !== over.id){
+      
+      const activeIndex = elements.findIndex((el) => el.id === active.id);
+      const overIndex = elements.findIndex((el) => el.id === over.id);
+      console.log(activeIndex, 'active');
+      console.log(overIndex, 'over');
+      
 
-    if (!active || !over) {
-      return;
+      const reorderedElements = arrayMove([...elements], activeIndex, overIndex);
+      setElements(reorderedElements);
+
     }
 
-    const activeIndex = elements.findIndex((el) => el.id === active.id);
-    const overIndex = elements.findIndex((el) => el.id === over.id);
+    // if (activeIndex === -1 || overIndex === -1) {
+    //   return;
+    // }
 
-    if (activeIndex === -1 || overIndex === -1) {
-      return;
-    }
-
-    const reorderedElements = arrayMove([...elements], activeIndex, overIndex);
-    setElements(reorderedElements);
+    
   };
 
   return (
@@ -39,7 +65,10 @@ export default function Puzzle() {
       onDragEnd={handleDragEnd}
     >
       <h1>ouiiii</h1>
-      <ImageDivide elements={elements} />
+      <div id={styles.plateau}>
+        <ImageDivide elements={elements} />
+      </div>
+      
     </DndContext>
   );
 }
