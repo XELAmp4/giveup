131937c748a1b69257f2cff76552ea5ecdaf95e0
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-10-23 16:10:17 +0200

    version puzzle grid

--- src/components/puzzle/sortableImage.js
+++ src/components/puzzle/sortableImage.js
@@ -2,21 +2,21 @@
 import React from 'react';
 import { useSortable } from '@dnd-kit/sortable';
 import PartImage from './partImage';
+import styles from './puzzle.module.sass';
 
 export default function SortableImage({ data, elements, setElements, imgWidth, imgHeight, key}) {
-  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: data.id });
-  console.log(data, 'SORTABLE');
-  
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: data.id });  
   const style = {
     ...data.style,
     transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
-    transition,
+    // Disable the transition property to prevent the element from returning to its initial position
+    transition: undefined,
   };
 
   return (
     <div
       id={data.id}
-      className={"item"}
+      className={styles[data.class]}
       ref={setNodeRef}
       {...attributes}
       {...listeners}
