53ab39930792dab4f951563edeabcdad29c6bb87
cenmelt <clement.collet123@orange.fr>
2023-10-23 17:03:20 +0200

    responsive

--- src/components/Timeline/explication/explication-timeline.js
+++ src/components/Timeline/explication/explication-timeline.js
@@ -7,7 +7,7 @@ export default function ExplicationTimeline({setGame}) {
   return( 
   <section className={styles.explication}>
     <h1>Petite explication</h1>
-    <p>Le but est simple mettre dans l'ordre chronologique (du plus vieux au plus récent) les covers des albums. Pour changer la position de deux cover c'est simple il suffis simplement de cliquer sur les deux covers.</p>
+    <p className={styles.exp}>Le but est simple mettre dans l'ordre chronologique (du plus vieux au plus récent) les covers des albums. Pour changer la position de deux cover c'est simple il suffis simplement de cliquer sur les deux covers.</p>
     <img src='Timeline/exemple-timeline.png' alt='Screen du jeu'></img>
     <Chrono 
       chrono={5}
