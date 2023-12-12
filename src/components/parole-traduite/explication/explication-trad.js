5e74d8a393c50f05722ca4a8efb078b943cc799a
cenmelt <clement.collet123@orange.fr>
2023-12-12 11:02:20 +0100

    Grosse avancé

---
+++ src/components/parole-traduite/explication/explication-trad.js
@@ -0,0 +1,20 @@
+import styles from './explication-trad.module.sass'
+import Chrono from '../../chrono/chrono';
+
+
+
+export default function ExplicationTrad({setGame}) {
+  return( 
+  <section className={styles.explication}>
+    <h1>Petite explication</h1>
+    <p className={styles.exp}>Tout le monde participe ! Utilisé les touches du clavier qui vous sont attribués pour buzzer et tenter votre chance. /!\ Attention si votre réponse est incorrect vous perderai des points. Bonne chance à vous !</p>
+    <img src='Timeline/exemple-traduction.png' alt='Screen du jeu Timeline'></img>
+    <Chrono 
+      chrono={5}
+      setGame={setGame}
+      redirection={'question-traduction'}
+      button={'Passer !'}
+    />
+  </section>
+  )
+}
