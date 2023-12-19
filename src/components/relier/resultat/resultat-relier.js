b53448eeaf3d34f7eafc8071f6b59e6410ef0fa4
Alexandre Bellet <alexandre.bellet0321@gmail.com>
2023-12-20 00:07:49 +0100

    save (reste bug artiste se melangent.......)

--- src/components/relier/resultat/resultat-relier.js
+++ src/components/relier/resultat/resultat-relier.js
@@ -2,14 +2,26 @@ import styles from './resultat-relier.module.sass'
 import { useState, useEffect } from 'react';
 
 
-export default function ResultatTimeline({Sources}) {
+export default function ResultatTimeline({Sources, save}) {
 //   const [date, setDate] = useState([Sources[0].Sortie, Sources[1].Sortie, Sources[2].Sortie]);
 //   const [img, setImg] = useState([Sources[0].img, Sources[1].img, Sources[2].img]);
 
+  console.log("%cRésultats", "color: red")
+  console.log(save);
+  console.log(Sources);
+
+  let juste;
+  console.log(save.length);
+  if (save.length !=3) {
+      juste = "FAUX"
+  }else{
+    juste = 'jsp wsh'
+  }
   return( 
   <section className={styles.resultat}>
     <h1>La réponse est ... relier</h1>
     
+    <p>{juste}</p>
   </section>
   )
 }
