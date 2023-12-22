fbe29282f691a4a0e5e4aaa8e1de85c2a3e5f860
lorenzo-bravard <103388200+lorenzo-bravard@users.noreply.github.com>
2023-12-22 19:04:27 +0100

    Correction fautes

--- src/components/relier/explication/explication-relier.js
+++ src/components/relier/explication/explication-relier.js
@@ -7,7 +7,7 @@ export default function ExplicationRelier({setGame}) {
   return( 
     <section className={styles.explication}>
     <h1>Petite explication</h1>
-    <p className={styles.exp}>L'objectif est de relier l'artiste a ca cover avant le temp imparti.</p>
+    <p className={styles.exp}>L'objectif est de relier l'artiste à sa cover avant le temps imparti.</p>
     <img src='imageExplication/exemple-relier.png' alt='Screen du jeu'></img>
     <Chrono 
       chrono={20}
